6abd26e39da781c4e9aa56da9fd606371d45163b
Illia Perevoznyk <fiatdoblo2009@gmail.com>
2019-04-07 22:40:34 +0300

    hw-3

--- javascript/module-2/js/script.js
+++ javascript/module-2/js/script.js
@@ -1,31 +1,28 @@
-//Task-1
-let input;
-const numbers = [];
-let total = 0;
+// //Task-1
+// let input;
+// const numbers = [];
+// let total = 0;
 
-while (input !== 0) {
-  input = prompt('Введите число');
-  input = Number(input);
-  if (Number.isNaN(input) === false) {
-    numbers.push(input);
-  }
-  if (Number.isNaN(input) === true) {
-    alert('Было введено не число');
-  }
-}
+// while (input !== 0) {
+//   input = Number(prompt('Введите число'));
+//   if (Number.isNaN(input) === false) {
+//     numbers.push(input);
+//   } else {
+//     alert('Было введено не число');
+//   }
+// }
 
-numbers.pop();
-console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
 
-if (numbers.length !== 0) {
-  for (let i = 0; i < numbers.length; i += 1) {
-    numbers[i] = Number(numbers[i]);
-    total += numbers[i];
-  }
-  console.log(`Общая сумма чисел равна ${total}`);
-}
+// if (numbers.length !== 0) {
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     total += numbers[i];
+//   }
+//   console.log(`Общая сумма чисел равна ${total}`);
+// }
 
-//Task-2
+// //Task-2
 
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let attemptsLeft = 3;
@@ -35,13 +32,11 @@ while (attemptsLeft > 0) {
   if (passwords.includes(input) === true) {
     alert('Добро пожаловать!');
     break;
-  }
-  if (passwords.includes(input) === false) {
+  } else {
     attemptsLeft -= 1;
     if (attemptsLeft > 0) {
       alert(`Неверный пароль, осталось ${attemptsLeft} попыток!`);
-    }
-    if (attemptsLeft === 0) {
+    } else {
       alert('У вас закончились попытки, аккаунт заблокирован!');
     }
   }
---
+++ javascript/module-3/js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const allLogins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = login => {
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isLoginUnique = (allLogins, login) => {
+  if (allLogins.includes(login)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addLogin = (allLogins, login) => {
+  if (isLoginValid(login) === true) {
+    if (isLoginUnique(allLogins, login) === true) {
+      allLogins.push(login);
+      return 'Логин успешно добавлен!';
+    } else {
+      return 'Такой логин уже используется!';
+    }
+  } else {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  }
+};
+
+const input = prompt('Введите логин:');
+
+console.log(addLogin(allLogins, input));
